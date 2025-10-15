3f37c4a90d9b2cb19f06a5273a85893389d2402c
bocarkaba1-dot <bocarkaba1@gmail.com>
2025-10-15 17:20:16 +0200

    Mise à jour du KPI seuil critique - ajustement de la requête

--- sql/kpis/KPI_Performance_Stock_produit_seuil_critique.sql
+++ sql/kpis/KPI_Performance_Stock_produit_seuil_critique.sql
@@ -1,3 +1,10 @@
+-- Stock des produits sous seuil critique : 
+--Identifier les produits dont le stock est faible pour éviter les ruptures.
+-- J'ai regardé dans la table des produit pour vérifier les quantitiés de stock
+-- J'ai fais le trie par quantité en ordre croissant  et cela permet de voir les quantité moins élevée au plus élevée.
+-- Cela permettra d'anciper les cmmmandes afin d'éviter les ruptures. 
+
+
 SELECT
 	productCode,
     productName,
